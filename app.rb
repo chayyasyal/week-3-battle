ce1c46399f939c921911ffcd51fd85de3b55e289
Chayya_Syal <chayya.syal@bbc.cuk>
2017-10-13 11:09:11 +0100

    Updated the instance variables to reference Plasyer instances instead of session

--- app.rb
+++ app.rb
@@ -8,16 +8,16 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = params[:player_1_name]
+    $player_1 = Player.new(params[:player_1_name])
     # session[:player_1_hit_points] = 100
-    $player_2 = params[:player_2_name]
+    $player_2 = Player.new(params[:player_2_name])
     # session[:player_2_hit_points] = 100
     redirect to('/play')
   end
 
   get '/play' do
-    @player_1_name = $player_1
-    @player_2_name = $player_2
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb(:play)
   end
 
@@ -28,8 +28,8 @@ class Battle < Sinatra::Base
   # end
 
   get '/attack' do
-    @player_1_name = $player_1
-    @player_2_name = $player_2
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb(:attack)
   end
 
